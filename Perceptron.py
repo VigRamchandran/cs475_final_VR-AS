ddbb9c67f527ee9fdcf06c32b8b62f654de54410
Vignesh Ramchandran <vramcha1@jhu.edu>
2016-12-04 02:45:35 -0500

    Added perceptron algorithm to test against training data

---
+++ Perceptron.py
@@ -0,0 +1,45 @@
+from cs475_types import ClassificationLabel, FeatureVector, Instance, Predictor
+import numpy as np
+
+class Perceptron:
+	def __init__(self):
+		self._w = None 
+		self._learning_rate = 1.0
+		self._iterations = 5.0
+		
+	def train(self, instances, training_labels):
+		I = self._iterations
+		eta = self._learning_rate
+		w = np.zeros(len(instances))
+		for k in range(I):
+			for i in range(len(instances)):
+				x = np.array(instances[i]) # ith example as np array
+				product = w.dot(x) # take dot product
+
+				y_hat = 0
+				if product >=  0:
+					y_hat = 1
+				else:
+					y_hat = -1
+
+				#update
+				correct_label = training_labels[i]
+				if y_hat != adjusted_label:
+					w +=  eta*adjusted_label*x
+
+		self._w = w
+
+		return self
+
+	def predict(self, instance):
+		w = self._w
+		x = np.array(instance) # instance feature vector. Will not have more features than training set.
+		product = w.dot(x)
+		
+		if product >= 0:
+			output = 1
+		else:
+			output = 0
+		return output
+
+
