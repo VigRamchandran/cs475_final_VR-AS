d63b7513ebae7f35a56efc114406d63194f5c78e
Anshul <asubra12@jhu.edu>
2016-12-06 14:19:25 -0500

    Time-dependent towers

--- objectTypes.py
+++ objectTypes.py
@@ -1,78 +1,133 @@
 # Contains class definitions for node objects used
 
 import numpy as np
+import pandas as pd
+import sys
+import math
+
 
 class Player:
-	def __init__(self):
-		self._match_id = None
-		self._hero = None
-		self._team = None
-		self._item0= None
-		self._item1 = None
-		self._item2 = None
-		self._item3 = None
-		self._item4 = None
-		self._item5 = None
+    def __init__(self):
+        self._match_id = None
+        self._hero = None
+        self._team = None
+        self._item0 = None
+        self._item1 = None
+        self._item2 = None
+        self._item3 = None
+        self._item4 = None
+        self._item5 = None
 
 
 class Match:
-	def __init__(self, players):
-		self._players = players
-		self._match_id = 0
-		self._tower_status_radiant = 0 #will be a 16-bit array
-		self._tower_status_dire = 0
-		self._barracks_status_radiant = 0 #will be an 8-bit array
-		self._barracks_status_dire = 0
-		self._label = 0
+    def __init__(self, players):
+        self._players = players
+        self._match_id = 0
+        self._tower_status_radiant = 0  # will be a 16-bit array
+        self._tower_status_dire = 0
+        self._barracks_status_radiant = 0  # will be an 8-bit array
+        self._barracks_status_dire = 0
+        self._label = 0
+        self._objectives = None
+        self._obj_feat = None
+        self._firstblood = 0  # 0 for radiant, 1 for dire
 
-	def get_feature_vector(self, case):
-		feature_vector = []
-		if case == 1:
+    def get_feature_vector(self, case):
+        feature_vector = []
+        if case == 1:
 
-			for player in self._players:
-				feature_vector.append(player._hero)
-				feature_vector.append(player._item0)
-				feature_vector.append(player._item1)
-				feature_vector.append(player._item2)
-				feature_vector.append(player._item3)
-				feature_vector.append(player._item4)
-				feature_vector.append(player._item5)
+            for player in self._players:
+                feature_vector.append(player._hero)
+                feature_vector.append(player._item0)
+                feature_vector.append(player._item1)
+                feature_vector.append(player._item2)
+                feature_vector.append(player._item3)
+                feature_vector.append(player._item4)
+                feature_vector.append(player._item5)
 
-			tower_radiant = self.bit2arr(self._tower_status_radiant)
-			tower_dire = self.bit2arr(self._tower_status_dire)
-			barracks_radiant = self.bit2arr(self._barracks_status_radiant)
-			barracks_dire = self.bit2arr(self._barracks_status_dire)
+            tower_radiant = self.bit2arr(self._tower_status_radiant)
+            tower_dire = self.bit2arr(self._tower_status_dire)
+            barracks_radiant = self.bit2arr(self._barracks_status_radiant)
+            barracks_dire = self.bit2arr(self._barracks_status_dire)
 
-			for num in tower_radiant:
-				feature_vector.append(num)
-			for num in tower_dire:
-				feature_vector.append(num)
-			for num in barracks_dire:
-				feature_vector.append(num)
-			for num in barracks_radiant:
-				feature_vector.append(num)
-		elif case == 2:
-			for player in self._players:
-				feature_vector.append(player._hero)
-				feature_vector.append(player._item0)
-				feature_vector.append(player._item1)
-				feature_vector.append(player._item2)
-				feature_vector.append(player._item3)
-				feature_vector.append(player._item4)
-				feature_vector.append(player._item5)
-		else:
-			print "Invalid case"
+            for num in tower_radiant:
+                feature_vector.append(num)
+            for num in tower_dire:
+                feature_vector.append(num)
+            for num in barracks_dire:
+                feature_vector.append(num)
+            for num in barracks_radiant:
+                feature_vector.append(num)
+        elif case == 2:
+            for player in self._players:
+                feature_vector.append(player._hero)
+                feature_vector.append(player._item0)
+                feature_vector.append(player._item1)
+                feature_vector.append(player._item2)
+                feature_vector.append(player._item3)
+                feature_vector.append(player._item4)
+                feature_vector.append(player._item5)
+        else:
+            print "Invalid case"
 
-		return feature_vector
+        return feature_vector
 
-	def get_label(self):
-		return int(self._label)
+    def transform_objectives(self, bin=300):
 
-	def bit2arr(self, bit_str): # converts a bit string to an array. 
-		arr = (",").join(bit_str).split(",")
-		for i in range(len(arr)):
-			arr[i] = int(arr[i])
+        buckets = int(math.ceil(7200.0/300))
 
-		return arr
+        features = [0]*6*(buckets)  # [0, 0, 0, 0, 0, 0] -> Rad Tower/Barracks/Roshan, Dire Tower/Barracks/Roshan
+        # Cap it out at 2 hr games. Past that assume no predictive capabilities because its based on so much.
+        fb = None
+
+        for index, row in self._objectives.iterrows():
+            if row['subtype'] == 'CHAT_MESSAGE_TOWER_KILL':
+                time = int(row['time'])
+                i,j = divmod(time, bin)
+                team = int(row['team'])-2
+                ind = (i*6)+(team*3)
+                if ind < len(features):
+                    features[ind] += 1
+                else:
+                    pass
+            if row['subtype'] == 'CHAT_MESSAGE_BARRACKS_KILL':
+                time = int(row['time'])
+                i, j = divmod(time, bin)
+                if row['key'] <= 32:  # Powers of Two, Dire->Radiant, Bottom -> Top, Melee-> Ranged.
+                    team = 1
+                else:
+                    team = 0
+                ind = (i*6) + (team * 3) + 1
+                if ind < len(features):
+                    features[ind] += 1
+                else:
+                    pass
+            if row['subtype'] == 'CHAT_MESSAGE_ROSHAN_KILL':
+                time = int(row['time'])
+                i,j = divmod(time, bin)
+                team = int(row['team'])-2
+                ind = (i*6)+(team*3) + 2
+                if ind < len(features):
+                    features[ind] += 1
+                else:
+                    pass
+            if row['subtype'] == 'CHAT_MESSAGE_FIRSTBLOOD':
+                if row['player1'] < 5:
+                    fb = 0
+                else:
+                    fb = 1
+
+        self._obj_feat = features
+        self._firstblood = fb
+        return
 
 
+    def get_label(self):
+        return int(self._label)
+
+    def bit2arr(self, bit_str):  # converts a bit string to an array.
+        arr = (",").join(bit_str).split(",")
+        for i in range(len(arr)):
+            arr[i] = int(arr[i])
+
+        return arr
--- read_data.py
+++ read_data.py
@@ -1,91 +1,106 @@
 import pandas as pd
-from objectTypes import Player, Match
+import objectTypes
+reload(objectTypes)
+from objectTypes import Match, Player
+
 
 def load_data(number_of_training_matches=1000):
-	if number_of_training_matches > 50000:
-		print "Data OVERLOADDDDDDDDD"
+    if number_of_training_matches > 50000:
+        print "Data OVERLOADDDDDDDDD"
 
-	path = "dota-2-matches/"
+    path = "dota-2-matches/"
 
-	player_data = pd.read_csv(path+"players.csv", 
-		usecols=["match_id", "hero_id", "player_slot", "item_0", "item_1", "item_2", "item_3", "item_4", "item_5"])
+    player_data = pd.read_csv(path + "players.csv",
+                              usecols=["match_id", "hero_id", "player_slot", "item_0", "item_1", "item_2", "item_3",
+                                       "item_4", "item_5"])
 
-	match_data = pd.read_csv(path + "match.csv",
-		usecols = ["match_id", "tower_status_radiant", "tower_status_dire", "barracks_status_radiant", "barracks_status_dire", "radiant_win"])
+    match_data = pd.read_csv(path + "match.csv",
+                             usecols=["match_id", "tower_status_radiant", "tower_status_dire",
+                                      "barracks_status_radiant", "barracks_status_dire", "radiant_win"])
 
-	matches = []
-	for i in range(number_of_training_matches): 
-		# initialize players in a given match
-		players = []
-		for j in range(10):
-			player = Player()
-			player._match_id = player_data["match_id"][(10*i + j)]
-			player._hero = player_data["hero_id"][(10*i + j)]
-			if player_data["player_slot"][(10*i + j)] < 15:
-				player._team = 0
-			else:
-				player._team = 1
-			player._item0 = player_data["item_0"][(10*i + j)]
-			player._item1 = player_data["item_1"][(10*i + j)]
-			player._item2 = player_data["item_2"][(10*i + j)]
-			player._item3 = player_data["item_3"][(10*i + j)]
-			player._item4 = player_data["item_4"][(10*i + j)]
-			player._item5 = player_data["item_5"][(10*i + j)]
-			players.append(player)
+    objectives_data = pd.read_csv(path + "objectives.csv",
+                                  usecols=["match_id", "key", "subtype", "team", "time", "value"])
 
-		# initialize matches
-		match = Match(players)
-		match._match_id = match_data["match_id"][i]
-		match._tower_status_radiant = "{0:b}".format(match_data["tower_status_radiant"][i]).zfill(16) # convert int to bit string and zero pad
-		match._tower_status_dire = "{0:b}".format(match_data["tower_status_dire"][i]).zfill(16)
-		match._barracks_status_radiant = "{0:b}".format(match_data["barracks_status_radiant"][i]).zfill(8)
-		match._barracks_status_dire = "{0:b}".format(match_data["barracks_status_dire"][i]).zfill(8)
-		match._label = match_data["radiant_win"][i]
+    matches = []
+    for i in range(number_of_training_matches):
+        # initialize players in a given match
+        players = []
+        for j in range(10):
+            player = Player()
+            player._match_id = player_data["match_id"][(10 * i + j)]
+            player._hero = player_data["hero_id"][(10 * i + j)]
+            if player_data["player_slot"][(10 * i + j)] < 15:
+                player._team = 0
+            else:
+                player._team = 1
+            player._item0 = player_data["item_0"][(10 * i + j)]
+            player._item1 = player_data["item_1"][(10 * i + j)]
+            player._item2 = player_data["item_2"][(10 * i + j)]
+            player._item3 = player_data["item_3"][(10 * i + j)]
+            player._item4 = player_data["item_4"][(10 * i + j)]
+            player._item5 = player_data["item_5"][(10 * i + j)]
+            players.append(player)
 
-		matches.append(match)
+        # initialize matches
+        match = Match(players)
+        match._match_id = match_data["match_id"][i]
+        match._tower_status_radiant = "{0:b}".format(match_data["tower_status_radiant"][i]).zfill(
+            16)  # convert int to bit string and zero pad
+        match._tower_status_dire = "{0:b}".format(match_data["tower_status_dire"][i]).zfill(16)
+        match._barracks_status_radiant = "{0:b}".format(match_data["barracks_status_radiant"][i]).zfill(8)
+        match._barracks_status_dire = "{0:b}".format(match_data["barracks_status_dire"][i]).zfill(8)
+        match._label = match_data["radiant_win"][i]
 
-	return matches
+        match._objectives = objectives_data.loc[objectives_data['match_id'] == i]  # Pass in a DataFrame
+
+        matches.append(match)
+
+    print 'Finished Loading'
+
+    return matches
 
 
-def generate_test_data(start=1001, number_of_points=50): # returns test Match objects
+def generate_test_data(start=1001, number_of_points=50):  # returns test Match objects
 
-	path = "dota-2-matches/"
+    path = "dota-2-matches/"
 
-	player_data = pd.read_csv(path+"players.csv", 
-		usecols=["match_id", "hero_id", "player_slot", "item_0", "item_1", "item_2", "item_3", "item_4", "item_5"])
+    player_data = pd.read_csv(path + "players.csv",
+                              usecols=["match_id", "hero_id", "player_slot", "item_0", "item_1", "item_2", "item_3",
+                                       "item_4", "item_5"])
 
-	match_data = pd.read_csv(path + "match.csv",
-		usecols = ["match_id", "tower_status_radiant", "tower_status_dire", "barracks_status_radiant", "barracks_status_dire", "radiant_win"])
+    match_data = pd.read_csv(path + "match.csv",
+                             usecols=["match_id", "tower_status_radiant", "tower_status_dire",
+                                      "barracks_status_radiant", "barracks_status_dire", "radiant_win"])
 
-	matches = []
-	for i in range(number_of_points): 
-		# initialize players in a given match
-		players = []
-		for j in range(10):
-			player = Player()
-			player._match_id = player_data["match_id"][(start - 1 + 10*i + j)]
-			player._hero = player_data["hero_id"][(start - 1 + 10*i + j)]
-			if player_data["player_slot"][(start - 1 + 10*i + j)] < 15:
-				player._team = 0
-			else:
-				player._team = 1
-			player._item0 = player_data["item_0"][(start - 1 + 10*i + j)]
-			player._item1 = player_data["item_1"][(start - 1 + 10*i + j)]
-			player._item2 = player_data["item_2"][(start - 1 + 10*i + j)]
-			player._item3 = player_data["item_3"][(start - 1 + 10*i + j)]
-			player._item4 = player_data["item_4"][(start - 1 + 10*i + j)]
-			player._item5 = player_data["item_5"][(start - 1 + 10*i + j)]
-			players.append(player)
+    matches = []
+    for i in range(number_of_points):
+        # initialize players in a given match
+        players = []
+        for j in range(10):
+            player = Player()
+            player._match_id = player_data["match_id"][(start - 1 + 10 * i + j)]
+            player._hero = player_data["hero_id"][(start - 1 + 10 * i + j)]
+            if player_data["player_slot"][(start - 1 + 10 * i + j)] < 15:
+                player._team = 0
+            else:
+                player._team = 1
+            player._item0 = player_data["item_0"][(start - 1 + 10 * i + j)]
+            player._item1 = player_data["item_1"][(start - 1 + 10 * i + j)]
+            player._item2 = player_data["item_2"][(start - 1 + 10 * i + j)]
+            player._item3 = player_data["item_3"][(start - 1 + 10 * i + j)]
+            player._item4 = player_data["item_4"][(start - 1 + 10 * i + j)]
+            player._item5 = player_data["item_5"][(start - 1 + 10 * i + j)]
+            players.append(player)
 
-		# initialize matches
-		match = Match(players)
-		match._match_id = match_data["match_id"][start - 1 + i]
-		match._tower_status_radiant = "{0:b}".format(match_data["tower_status_radiant"][start - 1 + i]).zfill(16)
-		match._tower_status_dire = "{0:b}".format(match_data["tower_status_dire"][start - 1 + i]).zfill(16)
-		match._barracks_status_radiant = "{0:b}".format(match_data["barracks_status_radiant"][start - 1 + i]).zfill(8)
-		match._barracks_status_dire = "{0:b}".format(match_data["barracks_status_dire"][start - 1 + i]).zfill(8)
-		match._label = match_data["radiant_win"][start - 1 + i]
+        # initialize matches
+        match = Match(players)
+        match._match_id = match_data["match_id"][start - 1 + i]
+        match._tower_status_radiant = "{0:b}".format(match_data["tower_status_radiant"][start - 1 + i]).zfill(16)
+        match._tower_status_dire = "{0:b}".format(match_data["tower_status_dire"][start - 1 + i]).zfill(16)
+        match._barracks_status_radiant = "{0:b}".format(match_data["barracks_status_radiant"][start - 1 + i]).zfill(8)
+        match._barracks_status_dire = "{0:b}".format(match_data["barracks_status_dire"][start - 1 + i]).zfill(8)
+        match._label = match_data["radiant_win"][start - 1 + i]
 
-		matches.append(match)
+        matches.append(match)
 
-	return matches
+    return matches
